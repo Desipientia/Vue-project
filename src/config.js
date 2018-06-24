ff324d9791bc37f04c7ba807d38a6aff49369fc7
deniallugo <deniallugo@gmail.com>
2018-06-24 08:54:39 +0300

    https over websocket

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const debug = process.env.NODE_ENV !== 'production';
 export const root = debug ? 'https://cid.legal' : '';
-export const wsRoot = 'ws://cid.legal/api/user/';
+export const wsRoot = 'wss://cid.legal/api/user/';
 
 export const projectName = 'cid';
